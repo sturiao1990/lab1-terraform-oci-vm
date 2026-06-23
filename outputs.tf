abc13506e7c0b53812fb369775cce42338b324ae
sturiao <andre.sturiao@gmail.com>
2026-06-23 14:37:16 -0300

    ajuste de output e sequencia na esteira

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
+output "instance_public_ip" {
+  description = "IP público da VM"
+  value       = oci_core_instance.vm_lab.public_ip
+}
+
 output "instance_private_ip" {
-  value = oci_core_instance.vm.private_ip
+  description = "IP privado da VM"
+  value       = oci_core_instance.vm_lab.private_ip
 }
 
 output "instance_id" {
-  value = oci_core_instance.vm.id
+  description = "OCID da VM"
+  value       = oci_core_instance.vm_lab.id
 }
